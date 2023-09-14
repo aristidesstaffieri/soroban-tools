37eec6341997cce841185974aed9269ed87082ac
Aristides Staffieri <aristides.staffieri@stellar.org>
2023-09-14 12:49:45 -0600

    fixes syntax for getting IDs, trims strings

--- cmd/crates/soroban-spec-typescript/ts-tests/src/test-token.ts
+++ cmd/crates/soroban-spec-typescript/ts-tests/src/test-token.ts
@@ -4,16 +4,22 @@ import { Address, Contract, networks } from "token";
 import fs from "node:fs";
 import process from "node:child_process";
 
+const tokenAId = fs.readFileSync(new URL("../contract-id-token-a.txt", import.meta.url), "utf8");
 const tokenBId = fs.readFileSync(new URL("../contract-id-token-b.txt", import.meta.url), "utf8");
 const rootStr = process.spawnSync("./soroban", ["config", "identity", "address"], { shell: true, encoding: "utf8" }).stdout;
-const aliceStr = process.spawnSync("./soroban", ["config", "identity", "alice", "address"], { shell: true, encoding: "utf8" }).stdout;
+const aliceStr = process.spawnSync("./soroban", ["config", "identity", "address", "alice"], { shell: true, encoding: "utf8" }).stdout;
 
-const root = new Address(rootStr);
-const alice = new Address(aliceStr);
+const root = new Address(rootStr.trim());
+const alice = new Address(aliceStr.trim());
 
-const tokenA = new Contract({ ...networks.standalone, rpcUrl, wallet });
+const tokenA = new Contract({
+  contractId: tokenAId.trim(),
+  networkPassphrase: networks.standalone.networkPassphrase,
+  rpcUrl,
+  wallet,
+});
 const tokenB = new Contract({
-  contractId: tokenBId,
+  contractId: tokenBId.trim(),
   networkPassphrase: networks.standalone.networkPassphrase,
   rpcUrl,
   wallet,
